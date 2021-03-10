ef5795f4462ae22afeb748e94791820407ffd15d
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-03-10 18:38:43 +0300

    Initial commit

---
+++ src/com/frost/Main.java
@@ -0,0 +1,37 @@
+package com.frost;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static Set<String> createSet() {
+        HashSet<String> set = new HashSet<String>();
+        set.add("л");
+        set.add("лл");
+        set.add("ллл");
+        set.add("лллл");
+        set.add("ллллл");
+        set.add("лллллл");
+        set.add("ллллллл");
+        set.add("лллллллл");
+        set.add("ллллллллл");
+        set.add("лллллллллл");
+        set.add("ллллллллллл");
+        set.add("лллллллллллл");
+        set.add("ллллллллллллл");
+        set.add("лллллллллллллл");
+        set.add("ллллллллллллллл");
+        set.add("лллллллллллллллл");
+        set.add("ллллллллллллллллл");
+        set.add("лллллллллллллллллл");
+        set.add("ллллллллллллллллллл");
+        set.add("лллллллллллллллллллл");
+        return set;
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
